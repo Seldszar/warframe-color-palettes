f2e5d50183a68a9863324de3a0b866713721d035
Seldszar <contact@seldszar.fr>
2017-01-19 17:48:42 +0100

    Added ESLint package and configuration files Added Travis configuration file Added required module extensions Added Editorconfig configuration for Markdown files

--- dist/index.js
+++ dist/index.js
@@ -5,91 +5,91 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.valentine = exports.twilight = exports.tenno = exports.storm = exports.smokeColors = exports.shamrock = exports.redWhiteBlue = exports.orokin = exports.lotus = exports.infested = exports.ice = exports.halloween = exports.grineer = exports.gamma = exports.fire = exports.eximus = exports.easter = exports.dojo = exports.daybreak = exports.corpus = exports.classicSatured = exports.classic = undefined;
 
-var _classic = require('../palettes/classic');
+var _classic = require('../palettes/classic.json');
 
 var _classic2 = _interopRequireDefault(_classic);
 
-var _classicSatured = require('../palettes/classic-satured');
+var _classicSatured = require('../palettes/classic-satured.json');
 
 var _classicSatured2 = _interopRequireDefault(_classicSatured);
 
-var _corpus = require('../palettes/corpus');
+var _corpus = require('../palettes/corpus.json');
 
 var _corpus2 = _interopRequireDefault(_corpus);
 
-var _daybreak = require('../palettes/daybreak');
+var _daybreak = require('../palettes/daybreak.json');
 
 var _daybreak2 = _interopRequireDefault(_daybreak);
 
-var _dojo = require('../palettes/dojo');
+var _dojo = require('../palettes/dojo.json');
 
 var _dojo2 = _interopRequireDefault(_dojo);
 
-var _easter = require('../palettes/easter');
+var _easter = require('../palettes/easter.json');
 
 var _easter2 = _interopRequireDefault(_easter);
 
-var _eximus = require('../palettes/eximus');
+var _eximus = require('../palettes/eximus.json');
 
 var _eximus2 = _interopRequireDefault(_eximus);
 
-var _fire = require('../palettes/fire');
+var _fire = require('../palettes/fire.json');
 
 var _fire2 = _interopRequireDefault(_fire);
 
-var _gamma = require('../palettes/gamma');
+var _gamma = require('../palettes/gamma.json');
 
 var _gamma2 = _interopRequireDefault(_gamma);
 
-var _grineer = require('../palettes/grineer');
+var _grineer = require('../palettes/grineer.json');
 
 var _grineer2 = _interopRequireDefault(_grineer);
 
-var _halloween = require('../palettes/halloween');
+var _halloween = require('../palettes/halloween.json');
 
 var _halloween2 = _interopRequireDefault(_halloween);
 
-var _ice = require('../palettes/ice');
+var _ice = require('../palettes/ice.json');
 
 var _ice2 = _interopRequireDefault(_ice);
 
-var _infested = require('../palettes/infested');
+var _infested = require('../palettes/infested.json');
 
 var _infested2 = _interopRequireDefault(_infested);
 
-var _lotus = require('../palettes/lotus');
+var _lotus = require('../palettes/lotus.json');
 
 var _lotus2 = _interopRequireDefault(_lotus);
 
-var _orokin = require('../palettes/orokin');
+var _orokin = require('../palettes/orokin.json');
 
 var _orokin2 = _interopRequireDefault(_orokin);
 
-var _redWhiteBlue = require('../palettes/red-white-blue');
+var _redWhiteBlue = require('../palettes/red-white-blue.json');
 
 var _redWhiteBlue2 = _interopRequireDefault(_redWhiteBlue);
 
-var _shamrock = require('../palettes/shamrock');
+var _shamrock = require('../palettes/shamrock.json');
 
 var _shamrock2 = _interopRequireDefault(_shamrock);
 
-var _smokeColors = require('../palettes/smoke-colors');
+var _smokeColors = require('../palettes/smoke-colors.json');
 
 var _smokeColors2 = _interopRequireDefault(_smokeColors);
 
-var _storm = require('../palettes/storm');
+var _storm = require('../palettes/storm.json');
 
 var _storm2 = _interopRequireDefault(_storm);
 
-var _tenno = require('../palettes/tenno');
+var _tenno = require('../palettes/tenno.json');
 
 var _tenno2 = _interopRequireDefault(_tenno);
 
-var _twilight = require('../palettes/twilight');
+var _twilight = require('../palettes/twilight.json');
 
 var _twilight2 = _interopRequireDefault(_twilight);
 
-var _valentine = require('../palettes/valentine');
+var _valentine = require('../palettes/valentine.json');
 
 var _valentine2 = _interopRequireDefault(_valentine);
 
